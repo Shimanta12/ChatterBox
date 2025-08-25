477663c8d87b7ce6a1e976d5498b352b7c0c966e
MD Newaz Sharif <92808885+mdnewazsharif@users.noreply.github.com>
2025-08-25 16:56:10 +0600

    chore: context provider modification

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -45,7 +45,6 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => { restore(); }, []);
 
-  // Higher order component to make the auth state available all over the application.
   return (
     <AuthContext.Provider value={{ user, setUser, login, logout, loadingInitial }}>
       {children}
